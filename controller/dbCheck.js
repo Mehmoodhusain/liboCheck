fe34a784a2c2e6eaa85e787205c5bba11c48cea4
Mehmoodhusain <mehmood.hussain@magnusmage.com>
2021-08-05 14:55:00 +0500

    dbCheck is working fine

--- controller/dbCheck.js
+++ controller/dbCheck.js
@@ -18,22 +18,23 @@ module.exports = {
 
   databaseCheck: function () {
     let done = true
-    let dbCheckCron = cron.schedule('*/5 * * * * *', async function () {
+    let count = 0
+    let lowerLimit = 1;
+    let upperLimit = 100000;
+    let low = 0;
+    let up = 0;
+    let dbCheckCron = cron.schedule('*/1 * * * * *', async function () {
       if (done == true) {
+        count+=1
         done = false
         let transaction = await Transaction.find();
-        let block = await Block.find().sort({
-          _id: 1
-        });
         let node = await Node.find();
         let supply = await Supply.find();
-        txsError = 0
+        let txsError = 0
         if (!transaction){
           console.log("TX NOT FOUND")
           txsError+=1
         }
-          
-
         if (!supply)
           console.log("SUPPLY NOT FOUND")
         else {
@@ -42,7 +43,7 @@ module.exports = {
             supplyError += 1
           }
         }
-
+        
         if (!node)
           console.log("NODE NOT FOUND")
         else {
@@ -51,29 +52,53 @@ module.exports = {
             nodeError += 1
           }
         }
-        if (!block)
-          console.log("BLOCK NOT FOUND")
-        else {
-          blockError = 0
-          for (let i = 0; i < block.length - 1; i++)
-            if (block[i].block.header.height != block[i + 1].block.header.height - 1) {
-              blockError += 1
-              console.log(block[i].block.header.height, "\t", block[i + 1].block.header.height)
+        
+        let x=true
+        let num = 1000
+        let skipp = lowerLimit - 1
+        let blockError = 0
+        var length = 0
+        while(x==true){
+          let block = await Block.find().limit(num).skip(skipp)
+          console.log(`Block.find().limit(${num}).skip(${skipp})`)
+          if(!block){
+            x=false
+            console.log("BLOCK NOT FOUND")
+          }
+          else{
+            if(length==0)
+              low = block[0].block.header.height
+            if(length==100000)
+              up = block[block.length-1].block.header.height
+            length+=block.length
+            for(let i=0;i<block.length-1;i++){
+              if(block[i].block.header.height!=block[i+1].block.header.height-1){
+                console.log(`${block[i].block.header.height},${block[i+1].block.header.height}`)
+                blockError+=1 
+              }
             }
+            if(block.length==num && length<=100000){
+              skipp+=num
+              block=[]
+            }
+              else{
+                x=false
+                upperLimit+=100000
+                lowerLimit+=100000
+              }
+          }
         }
         function Records(length, error) {
           this.LENGTH = length;
           this.ERRORS = error;
         }
-        
         let supplyRecord = new Records(supply.length, supplyError);
         let nodeRecord = new Records(node.length, nodeError);
-        let blockRecord = new Records(block.length, blockError);
+        let blockRecord = new Records(`${low},${up}`, blockError);
         let txsRecord = new Records(transaction.length, txsError)
-        
-        console.table({'SUPPLY':supplyRecord, 'NODE':nodeRecord, 'BLOCK':blockRecord, 'TRANSACTION':txsRecord});
-
-        done = true
+        console.table({'SUPPLY':supplyRecord, 'NODE':nodeRecord, 'BLOCK': blockRecord, 'TRANSACTION':txsRecord})
+        done=true
+               
       }
     });
   },
